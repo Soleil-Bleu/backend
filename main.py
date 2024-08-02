3a86daf5baaf927e2c50a8f8fe01022f5a6b1b2b
Twane70 <tonduantoine@gmail.com>
2024-08-02 16:18:01 +0200

    fix: remove uvicorn reload

--- main.py
+++ main.py
@@ -212,4 +212,4 @@ async def calc_simulation(
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
